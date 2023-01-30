6082ec6da2f32c69d8b5be4607ee0224a6b4c867
wilsonchoque <wilsonchoque93@gmail.com>
2023-01-30 11:57:49 -0500

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-2023-bf783',
-    appId: '1:585900590228:web:fa2afa88f6b7b45204e081',
-    storageBucket: 'angular-2023-bf783.appspot.com',
-    apiKey: 'AIzaSyDPvtH3wzVuhoZNqwPwyQqRC9yOVpuljv4',
-    authDomain: 'angular-2023-bf783.firebaseapp.com',
-    messagingSenderId: '585900590228',
+    projectId: 'angular-4606c',
+    appId: '1:633178703255:web:d7907537e8956e6de030d2',
+    storageBucket: 'angular-4606c.appspot.com',
+    apiKey: 'AIzaSyC5e0OHjPgcayGgQqbGQ4BJ4w--0JgcVcg',
+    authDomain: 'angular-4606c.firebaseapp.com',
+    messagingSenderId: '633178703255',
   },
 }
